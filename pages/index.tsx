8277c4b08a027608a1b62fb06569aea5c20d610a
Pablo Satler <pablosatler@gmail.com>
2021-01-25 22:34:29 -0300

    chore: image meta tag changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -43,14 +43,14 @@ export default function Home() {
         <meta property="og:url" content="https://metatags.io/"/>
         <meta property="og:title" content="Capixabês — Quiz sobre girias e palavras faladas pelo capixaba"/>
         <meta property="og:description" content="Teste seus conhecimentos sobre gírias e palavras faladas pelos capixadas!"/>
-        <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"/>
+        <meta property="og:image" content="https://www.segueviagem.com.br/wp-content/uploads/2019/11/praia-de-camburi_vitoria_setur_yuri-barichivich-2-1200x675.jpg"/>
 
         {/* <!-- Twitter --> */}
         <meta property="twitter:card" content="summary_large_image"/>
         <meta property="twitter:url" content="https://metatags.io/"/>
         <meta property="twitter:title" content="Capixabês — Quiz sobre girias e palavras faladas pelo capixaba"/>
         <meta property="twitter:description" content="Teste seus conhecimentos sobre gírias e palavras faladas pelos capixadas!"/>
-        <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"/>
+        <meta property="twitter:image" content="https://www.segueviagem.com.br/wp-content/uploads/2019/11/praia-de-camburi_vitoria_setur_yuri-barichivich-2-1200x675.jpg"/>
       </Head>
       <QuizBackground backgroundImage={db.bg}>
           <QuizContainer>
